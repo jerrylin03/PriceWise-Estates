bbef44f4db915c37edf24b5d508dd004a5fb9cec
Jerry Lin <lin.jerry03@gmail.com>
2024-01-29 15:06:01 -0500

    fix: revise PR comments
    - change query string to use input
    - load API key from .env
    - prompt for user profile update

--- scanner_cli.py
+++ scanner_cli.py
@@ -7,22 +7,26 @@ PriceWise Estates CLI
 import argparse
 import json
 import csv
+import os
+from dotenv import load_dotenv
 from pip._vendor import requests
 
-API_KEY = ""
+# Load environment variables from .env file
+load_dotenv()
+
+RAPID_API_KEY = os.getenv("RAPID_API_KEY")
 
 def fetch_top_school_districts(state_code):
     """ Function to get the top 10 school districts in a state saved to a json file """
-    api_url = "https://schooldigger-k-12-school-data-api.p.rapidapi.com/v2.0/rankings/districts/VA"
+    api_url = "https://schooldigger-k-12-school-data-api.p.rapidapi.com/v2.0/rankings/districts/" + state_code
 
     headers = {
-        "X-RapidAPI-Key": API_KEY,
+        "X-RapidAPI-Key": RAPID_API_KEY,
         "X-RapidAPI-Host": "schooldigger-k-12-school-data-api.p.rapidapi.com"
     }
 
     try:
         response = requests.get(api_url, headers=headers)
-        print(response.raise_for_status())
 
         district_list = response.json()["districtList"]
 
@@ -56,10 +60,10 @@ def fetch_properties(state_code):
     """ Function to get the newest 42 properties for sale by state saved to a json file """
     api_url = "https://us-real-estate.p.rapidapi.com/v3/for-sale"
 
-    querystring = {"state_code":"VA","sort":"newest","offset":"0"}
+    querystring = {"state_code":state_code,"sort":"newest","offset":"0"}
 
     headers = {
-        "X-RapidAPI-Key": API_KEY,
+        "X-RapidAPI-Key": RAPID_API_KEY,
         "X-RapidAPI-Host": "us-real-estate.p.rapidapi.com"
     }
 
@@ -111,36 +115,55 @@ def store_user_data(username):
     None: Updates the json file user_data.json
     """
     # Load existing data from file
+
+    update = "N"
+
     try:
         with open('user_data.json', 'r', encoding="utf8") as file:
             user_data = json.load(file)
         # Check if the username is already in the database
         if username in user_data:
             print(f"Hello, {username}!")
+            # print out current user profile
+            username_data = user_data[username]
+            print("Here is the current user profile")
+            print(f"Real Estate Minimum Price: ${username_data['min_price_range']}")
+            print(f"Real Estate Maximum Price: ${username_data['max_price_range']}")
+            print(f"School District Rating Preference: {username_data['school_rating_preference']}/10")
+            print(f"Gross Monthly Income: ${username_data['gross_income']}")
+            print(f"Monthly Debt Payments: ${username_data['current_debt']}")
+            print(f"Property Management fees as percent of rental income: {username_data['current_debt']}%")
+            print(f"Maintenance costs as percent of rental income: {username_data['maintenance_costs']}%")
+            print(f"Insurance costs as percent of rental income: {username_data['insurance']}%")
+            # ask if we want to update
+            update = input("Do you want to update the user profile ? (Y/N) ")
         else:
             print(f"New user {username}")
     except FileNotFoundError:
         # Create a new user_data dictionary if the file doesn't exist
         user_data = {}
         print(f"New user {username}")
+        update = "Y"
 
-    # Ask for and save user input
-    user_data[username] = {
-        'min_price_range': float(input("Enter your minimum price $: ")),
-        'max_price_range': float(input("Enter your maximum price $: ")),
-        'school_rating_preference': int(input("Enter your school rating preference (1-5): ")),
-        'gross_income': float(input("Enter your gross income $: ")),
-        'current_debt': float(input("Enter your current debt $: ")),
-        'property_management_fees': float(input("Enter property management fees as percent of income: ")),
-        'maintenance_costs': float(input("Enter maintenance costs as percent of income: ")),
-        'insurance': float(input("Enter insurance costs: "))
-    }
+    if update.upper() == "Y":
+        # Ask for and save user input
+        user_data[username] = {
+            'min_price_range': float(input("Enter your minimum price $: ")),
+            'max_price_range': float(input("Enter your maximum price $: ")),
+            'school_rating_preference': int(input("Enter your school rating preference (1-10): ")),
+            'gross_income': float(input("Enter your gross monthly income $: ")),
+            'current_debt': float(input("Enter your current monthly debt payments $: ")),
+            'property_management_fees': int(input("Enter property management fees as percent of income: ")),
+            'maintenance_costs': int(input("Enter maintenance costs as percent of income: ")),
+            'insurance': int(input("Enter insurance costs as percent of rental income: "))
+        }
 
-    # Save the updated user data
-    with open('user_data.json', 'w', encoding="utf8") as file:
-        json.dump(user_data, file, indent=2)
+        # Save the updated user data
+        with open('user_data.json', 'w', encoding="utf8") as file:
+            json.dump(user_data, file, indent=2)
 
-    print(f"User data for {username} has been updated.")
+        print(f"User data for {username} has been updated.")
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="PriceWise Estates CLI")
